230019164233a35822ec8d77077c886b6d0a5f51
tuanngoc1601 <tuanngoc12340@gmail.com>
2023-02-10 12:02:27 +0000

    smart phone

--- db/migrate/20230112083120_create_books.rb
+++ db/migrate/20230112083120_create_books.rb
@@ -1,8 +1,14 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
-      t.string :title
-      t.integer :number_of_pages
+      t.integer :id
+      t.string :name
+      t.text :description
+      t.integer :price
+      t.string :color
+      t.integer :quantity
+      t.integer :id_producer
+      
 
       t.timestamps
     end
